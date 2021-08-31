7fbb4aef3bf9dafa4e4046bb10f56f35d968ef3a
tarunsinghtanwar <tarunsinghtanwar14@gmail.com>
2021-08-31 20:15:44 +0530

    code refactor and added new expense option

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,22 +2,18 @@ import react,{useState} from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate.js';
 function ExpenseItem(props) {
-	const [title,setTitle]=useState(props.title)
-	const newTitle=()=>{
-		setTitle("Hacked")
-		
-	}
+	
 	
 	return (
 		<div className="expense-item">
 			<ExpenseDate date={props.date}/>
 
 			<div className="expense-item__description">
-				<h2>{title}</h2>
+				<h2>{props.title}</h2>
 			</div>
 
-			<div className="expense-item__price">{props.amount}</div>
-			<button onClick={newTitle}>Update</button>
+			<div className="expense-item__price">${props.amount}</div>
+			
 		</div>
 	);
 }
